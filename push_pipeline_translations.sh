0406ef448f6a93d535c612edd55234eda7185df9
Aaron Chen <aaron@Aarons-MacBook-Pro.local>
2015-06-04 14:42:38 +0800

    update the script to work with TVT returns

--- push_pipeline_translations.sh
+++ push_pipeline_translations.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# Crated by Aaron Chen
+# Created by Aaron Chen
 
 DIR=${1%%/}
+
+OSTHANES=$(pwd)
+USER=$(git config user.name)
 MISMATCH=
 
 if [ -z $DIR ]; then
@@ -14,24 +17,26 @@ if [ ! -d "$DIR" ]; then
 	exit 1
 fi
 
-USER=$(git config user.name)
 if [ -z "$USER" ]; then
 	echo -e "Please set your user name by running: git config user.name \"your_name\""
 	exit 1
 fi
 
-echo -e "### Check if the contents of translation directory (${DIR}) matches with the repository ###"
+echo "### Check if the contents of translation directory matches with the repository ###"
+echo -e "Git:\n\t${OSTHANES}"
+echo -e "Translation:\n\t${DIR}"
+cd "${DIR}"
 for i in $(ls -d */); do
 	ext_dir=${i%%/}
-	echo -e "- ${ext_dir}"
+	echo -e "\n--- ${ext_dir} ---"
 	
-	if grep "${ext_dir}" .gitignore >/dev/null; then
+	if grep "${ext_dir}" ${OSTHANES}/.gitignore >/dev/null; then
 		echo " >>> Skipped"
 		continue
 	fi
 	
-	git_files=$(ls -1 "$ext_dir")
-	translation_files=$(ls -1 "${DIR}/$ext_dir")
+	git_files=$(ls -1 "${OSTHANES}/$ext_dir" 2>/dev/null)
+	translation_files=$(ls -1 "$ext_dir" 2>/dev/null)
 	
 	if [ "$git_files" == "$translation_files" ]; then
 		echo " >>> Matched"
@@ -42,19 +47,27 @@ for i in $(ls -d */); do
 		MISMATCH=1
 	fi
 done
+cd "${OSTHANES}"
 
 if [ $MISMATCH ]; then
-	echo -e "\nPlease correct mismatched files above and run the command again!\n"
-	exit 1
-else
-	echo -e "\n### Copy files from translation directory ($DIR) to the repository ###"
-	cp -R "${DIR}/" ./ 
-	
-	echo -e "\n### Push to the repository ###"
-	git add * 
-	git commit -m "Updating manually translated files on $(date) by ${USER}"
-	git push
-	
+	echo -e "\nPlease take a moment to review the mismatched files above!\n"
+	read -r -p "Do you want to continue the push? [y/N] " response
+	if ! [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+		echo -e "\nExit!\n"
+	    exit 1
+	fi
 fi
+
+echo -e "\n### Copy files from translation directory to the repository ###"
+cp -R "${DIR}/" ./ 
+	
+echo -e "\n### Push to Github ###"
+git add *
+git commit -m "Updating manually translated files on $(date) by ${USER}"
+git push
+
+echo -e "\nDone!\n"
+	
+
 
 
